489d0cd0244158bf2b136f9662a6161fca0148dd
VishalRai7 <raiv23656@gmail.com>
2025-06-20 16:56:21 +0530

    Update OnboardingPage.jsx

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -39,7 +39,7 @@ const OnboardingPage = () => {
 
   const handleRandomAvatar = () => {
     const idx = Math.floor(Math.random() * 100) + 1; // 1-100 included
-    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+    const randomAvatar = `https://api.dicebear.com/6.x/adventurer/svg?seed=${idx};
 
     setFormState({ ...formState, profilePic: randomAvatar });
     toast.success("Random profile picture generated!");
